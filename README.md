7aa2ca6c70a33c628e389bc87e1aa08a6b56e787
Sean Anthony <seananthony@Seans-MacBook-Pro.local>
2022-10-11 13:44:56 -0700

    first draft of W4 homework

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Week-4Homework1
+
+## Description:
+
+This weeks assignment was a 5 day weather forecast, that also stores saved cities underneath the search bar. I linked the api from the open weather website. I made the api a variable then later used a function, so that anytime a city is searched the javascript searches for the https, + the city name that is placed in the search bar, I then added a request in the url to provide the imperial measurments, because thats what I am used to seeing and what I expected was wanted. At the end of the https the api is added, to make it a complete website whose information could be pulled for the weather app. I'm still working on storing the saved cities data, I'm having an issue with all the past cities being searched again as opposed to the current only. I am also trying to get the 5 day forecast. I started using the 1 day API so I may have to start from scratch.
+
+## Screenshots:
+
+![A weather app that displays current time day and temp as well as stores searched cities below search bar](assets/Screen%20Shot%202022-10-11%20at%201.41.02%20PM.png)
+
+## Links:
+
+deployed application:
+git hub repo: https://github.com/phillipanthony11/Week-4Homework1
---
+++ script.js
@@ -0,0 +1,101 @@
+var api = "c12a9ed4e6eb7ddfd381ba335aa7ed2e"
+var cityName = document.querySelector('#cityName')
+var temperature = document.querySelector('#temperature')
+var wind = document.querySelector('#wind')
+var humidity = document.querySelector('#humidity')
+var icon = document.querySelector("#icon")
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1; //months from 1-12
+var day = dateObj.getUTCDate();
+var year = dateObj.getUTCFullYear();
+var newdate = `${month}/${day}/${year}`;
+var cities = []
+var ul = document.getElementById("citiesList");
+
+function initializeCities(){
+    if(JSON.parse(localStorage.getItem("cities"))){
+        var savedCities = JSON.parse(localStorage.getItem("cities"))
+        cities = savedCities
+        for (let i = 0; i < cities.length; i++) {
+            var li = document.createElement("li");
+            li.innerHTML = cities[i]
+            ul.appendChild(li); 
+        }
+    }
+}
+
+initializeCities()
+
+document.querySelector("#searchButton").addEventListener('click', search)
+
+function search () {
+    getWeather(document.querySelector("#search").value)
+}
+
+function saveCity(city){
+    cities.push(city)
+    localStorage.setItem("cities", JSON.stringify(cities))
+    var li = document.createElement("li");
+    li.innerHTML = city
+    ul.appendChild(li); 
+    var listItems = document.querySelectorAll("#citiesList li")
+    listItems.forEach(addClick)
+}
+
+function addClick(listItem){
+    listItem.addEventListener("click", function(event){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+event.target.innerHTML+"&units=imperial&appid=" + api)
+        .then((function(response) {
+            if(response.ok){
+                response.json()
+                .then(function (data) {
+                    var iconCode = data.weather[0].icon
+                    var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+                    icon.setAttribute("src", iconurl)
+                    icon.style.display = "block"
+                    cityName.innerHTML = event.target.innerHTML + " (" + newdate + ")"
+                    temperature.innerHTML = "Temperature: "  + data.main.temp + "°F"
+                    humidity.innerHTML = "Humidity: " + data.main.humidity + "%"
+                    wind.innerHTML = "Wind Speed: " + data.wind.speed + "MPH"
+                    console.log(data);
+                    saveCity(event.target.innerHTML)
+                  });
+            }
+            else{
+                console.log(response)
+                alert('Error: ' + response.statusText)
+            }
+    }))
+})}
+
+function getWeather(city) {
+    console.log(city)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=" + api)
+    .then((function(response) {
+        if(response.ok){
+            response.json()
+            .then(function (data) {
+                var iconCode = data.weather[0].icon
+                var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+                icon.setAttribute("src", iconurl)
+                icon.style.display = "block"
+                cityName.innerHTML = city + " (" + newdate + ")"
+                temperature.innerHTML = "Temperature: "  + data.main.temp + "°F"
+                humidity.innerHTML = "Humidity: " + data.main.humidity + "%"
+                wind.innerHTML = "Wind Speed: " + data.wind.speed + "MPH"
+                console.log(data);
+                saveCity(city)
+              });
+        }
+        else{
+            console.log(response)
+            alert('Error: ' + response.statusText)
+        }
+    }))
+
+}
+
+
+// Edge cases:
+// 1. Same city multiple times
+// 2. Make cities appear on load 
